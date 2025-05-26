cbd5358ea4808c3ad39691aed84472596662b764
Matin <Matin@DESKTOP-QUGOMAD>
2025-05-26 17:11:42 +0330

    Add project files.

---
+++ 378daftarkhoneh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _378daftarkhoneh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
